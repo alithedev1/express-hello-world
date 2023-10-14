623c86d80f871f20de0537ebffa5e8c37fa0bceb
Ali the Dev <105778118+alithedev1@users.noreply.github.com>
2023-10-14 12:53:22 +0500

    Changes

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const port = 8000
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World haha')
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('set username', (username) => {
+    socket.username = username;
+    io.emit('user joined', username);
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.username) {
+      io.emit('user left', socket.username);
+    }
+  });
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', { username: socket.username, message });
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
