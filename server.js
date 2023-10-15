3d128a3e6dc4623fcb6f38d42ddcdc3189e3a822
Ali the Dev <105778118+alithedev1@users.noreply.github.com>
2023-10-15 19:06:39 +0500

    Changes

--- server.js
+++ server.js
@@ -2,22 +2,41 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb+srv://jacksparrow:BJHdgjsagkjgsahRHGf@cluster0.ohjvwzw.mongodb.net';
+const client = new MongoClient(uri);
 
 const port = process.env.PORT || 3000;
-
 const app = express();
 const server = http.createServer(app);
 
-app.get('/data', (req, res) => {
-  res.send({
-    name: 'Ali',
-    age : '20'
-  })
-})
+const dbName = 'World';
+const collectionName = 'People';
 
 app.use(cors());
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
+app.get('/', async (req, res) => {
+  try {
+      // Connect to the MongoDB server
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection(collectionName);
+
+      // Retrieve all student data from the collection
+      const data = await collection.find({}).toArray();
+
+      // Render the homepage with the student data
+      res.send(data);
+  } catch (error) {
+      // If an error occurs, render an error page
+      res.send('No page found', error);
+  } finally {
+      // Close the MongoDB client connection
+      await client.close();
+  }
+});
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
